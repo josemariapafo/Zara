9d3377529ccfedf1f098fb78359f5025ccaa3ac8
josemariapafo <parrillajosemaria@gmail.com>
2024-02-10 12:47:08 +0100

    Se introduce swagger

--- src/main/java/com/inditex/zara/domain/service/PriceService.java
+++ src/main/java/com/inditex/zara/domain/service/PriceService.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.Optional;
 
 
 /**
@@ -18,23 +17,11 @@ import java.util.Optional;
 @AllArgsConstructor
 public class PriceService implements PriceUseCase {
 
-
     private final PriceOutputPort priceOutputPort;
 
     @Override
     public Price getPriceInformationByDateAndProductBrandId(Date applyDate, Long productId, Long brandId) {
-        Optional<Price> price = priceOutputPort.getPriceByBrandProductAndDate(applyDate, productId, brandId);
-        isEmptyResult(price);
-        return price.get();
-    }
-
-    /**
-     * isEmptyResult
-     * @param price
-     */
-    private void isEmptyResult(Optional<Price> price) {
-        if(!price.isPresent()) {
-            throw new NotFoundException("No se encontraron resultados.");
-        }
+        return priceOutputPort.getPriceByBrandProductAndDate(applyDate, productId, brandId)
+                .orElseThrow(() -> new NotFoundException("No se encontraron resultados."));
     }
 }
